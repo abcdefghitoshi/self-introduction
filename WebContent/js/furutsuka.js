92141d77114647481867558c04441b22b39ce3aa
abcdefghitoshi <h.furutsuka@a-fource.co.jp>
2023-04-21 16:28:56 +0900

    eclipseからのfirst commit

---
+++ WebContent/js/furutsuka.js
@@ -0,0 +1,62 @@
+//線が伸びるための設定を関数でまとめる
+function ScrollTimelineAnime(){
+	$('.timeline li').each(function(){// それぞれのli要素の
+		var elemPos = $(this).offset().top;// 上からの高さ取得
+		var scroll = $(window).scrollTop();// スクロール値取得
+		var windowHeight = $(window).height();// windowの高さ取得
+		var startPoint = 400; //線をスタートさせる位置を指定※レイアウトによって調整してください
+		if (scroll >= elemPos - windowHeight-startPoint){				
+			var H = $(this).outerHeight(true)//liの余白と高さを含めた数値を取得
+			//スクロール値から要素までの高さを引いた値を、liの高さの半分のパーセントで出す
+			var percent = (scroll+startPoint - elemPos) / (H/2) *100;//liの余白と高さの半分で線を100％に伸ばす
+
+			// 100% を超えたらずっと100%を入れ続ける
+			if(percent  > 100){
+				percent  = 100;
+			}
+			// ボーダーの長さをセット
+			$(this).children('.border-line').css({
+				height: percent + "%", //CSSでパーセント指定
+			});
+		} 
+	});
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).on('scroll', function(){
+	ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
+});
+
+
+// -------------スライダーのjs-----------------
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function(){
+	ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
+});
+
+$('.slider').slick({
+		arrows: false,//左右の矢印はなし
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+		speed: 6900,//スライドのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+		pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+		cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+		slidesToShow: 4,//スライドを画面に4枚見せる
+		slidesToScroll: 1,//1回のスライドで動かす要素数
+		responsive: [
+			{
+			breakpoint: 769,//モニターの横幅が769px以下の見せ方
+			settings: {
+				slidesToShow: 2,//スライドを画面に2枚見せる
+			}
+		},
+		{
+			breakpoint: 426,//モニターの横幅が426px以下の見せ方
+			settings: {
+				slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+			}
+		}
+	]
+	});
